e238d0e651dddd05d3141a3be5b3455984645b65
Patrick O'Connor <pattyo20@optonline.netm>
2023-02-16 17:28:28 -0500

    initial commit after project set up

---
+++ src/styles/header.styled.js
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+
+export const StyledHeader = styled.header`
+    background-color: #032c48;
+
+    h1 {
+        color: #ffffff
+    }
+
+    &:hover {
+        background-color: red;
+    }
+`
