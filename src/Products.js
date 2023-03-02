c33e1c8fc7e9e949caa9dd3b58c22ed9351eb430
Patrick O'Connor <pattyo20@optonline.netm>
2023-03-02 16:24:05 -0500

    started builidng out featured products component with ebay api (some issues though)

--- src/Products.js
+++ src/Products.js
@@ -6,7 +6,9 @@ const ParentComponent = () => {
 
   useEffect(() => {
     // Make API call to retrieve the products data
-    fetch("https://example.com/products")
+    fetch(
+      "https://svcs.ebay.com/services/search/FindingService/v1?OPERATION-NAME=findItemsByKeywords&SERVICE-VERSION=1.0.0&SECURITY-APPNAME=SBX-d75429346-e2546562&RESPONSE-DATA-FORMAT=JSON&REST-PAYLOAD&keywords=samsung%20tv&paginationInput.entriesPerPage=4"
+    )
       .then((response) => response.json())
       .then((data) => setProducts(data));
   }, []);
--- src/components/Featuredproducts.js
+++ src/components/Featuredproducts.js
@@ -1,22 +1,49 @@
-/* eslint-disable react/no-unescaped-entities */
-import React from "react";
+/* eslint-disable no-magic-numbers */
+/* eslint-disable no-console */
+import React, { useState, useEffect } from "react";
 import {
   FeaturedProductsContainer,
   FeaturedProductsImage,
   FeaturedProductsImageWrapper,
 } from "../styles/Featuredproducts.styled";
-import SamsungTV from "../assets/images/SamsungTV.jpg";
 
 const FeaturedProducts = () => {
+  const [items, setItems] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const url = "https://svcs.ebay.com/services/search/FindingService/v1";
+      const params = new URLSearchParams({
+        "OPERATION-NAME": "findItemsByKeywords",
+        "SERVICE-VERSION": "1.0.0",
+        "SECURITY-APPNAME": "SBX-d75429346-e2546562",
+        "RESPONSE-DATA-FORMAT": "JSON",
+        "REST-PAYLOAD": true,
+        keywords: "samsung tv",
+        "paginationInput.entriesPerPage": 4,
+      });
+      const response = await fetch(`${url}?${params}`, { mode: "no-cors" });
+      const data = await response.json();
+
+      console.log(data);
+      setItems(data.findItemsByKeywordsResponse[0].searchResult[0].item);
+    };
+
+    fetchData();
+  }, []);
+
   return (
     <>
       <FeaturedProductsContainer>
         <h1>Featured Products</h1>
         <FeaturedProductsImageWrapper>
-          <FeaturedProductsImage src={SamsungTV} alt="Samsung TV" />
-          <FeaturedProductsImage src={SamsungTV} alt="Samsung TV" />
-          <FeaturedProductsImage src={SamsungTV} alt="Samsung TV" />
-          <FeaturedProductsImage src={SamsungTV} alt="Samsung TV" />
+          {items.map((item) => (
+            <FeaturedProductsImage
+              key={item.itemId[0]}
+              src={item.galleryURL[0]}
+              alt={item.title[0]}
+            />
+          ))}
         </FeaturedProductsImageWrapper>
       </FeaturedProductsContainer>
     </>
